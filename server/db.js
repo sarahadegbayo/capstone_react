3d9d3d038f809f8ce8d10e14d6bb8d5719aff917
sarahadegbayo <adegbayosarah2021@gmail.com>
2025-02-11 04:27:05 -0500

    mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:<6CEIqod18OH8Q8kmqIeVyYkG>@172.21.136.205:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
